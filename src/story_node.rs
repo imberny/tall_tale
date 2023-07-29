a655fca691c36f21489282ed8c4a8a0d98a7b9b0
Bernard Cloutier <bernard.p.cloutier@gmail.com>
2023-07-28 21:46:05 -0400

    Switch story node structure to graph

--- src/story_node.rs
+++ src/story_node.rs
@@ -1,38 +1,39 @@
 use itertools::Itertools;
-use std::{collections::HashMap, fmt::Display, ops::Range, rc::Rc};
+use petgraph::prelude::{Graph, NodeIndex};
+use serde::{Deserialize, Serialize};
+use std::{collections::HashMap, fmt::Display, ops::Range};
 
-pub const ID: &str = "id";
-
-pub type Id = i64;
 pub type Alias = String;
+pub type AliasIndex = usize;
+pub type EntityIndex = usize;
 pub type PropertyName = String;
 pub type PropertyMap = HashMap<PropertyName, Property>;
 // key is a pair of ids, value is property from POV of 1st entity
-pub type RelationMap = HashMap<(Id, Id), PropertyMap>;
+pub type RelationMap = HashMap<(EntityId, EntityId), PropertyMap>;
 
-pub struct Entity(PropertyMap);
-impl Entity {
-    pub fn builder(id: Id) -> EntityBuilder {
-        EntityBuilder::new(id)
-    }
+#[derive(Hash, PartialEq, Eq, Clone, Copy)]
+pub struct StoryId(usize);
 
-    fn new(id: Id) -> Self {
-        Self(PropertyMap::from([(ID.into(), id.into())]))
-    }
+#[derive(Hash, PartialEq, Eq, Clone, Copy)]
+pub struct EntityId(usize);
 
-    fn insert(&mut self, property_name: PropertyName, property: Property) {
-        self.0.insert(property_name, property);
-    }
-
-    pub fn get(&self, property_name: impl Into<PropertyName>) -> Option<&Property> {
-        self.0.get(&property_name.into())
+impl EntityId {
+    pub const fn new(id: usize) -> Self {
+        Self(id)
     }
 }
 
-pub struct EntityBuilder(Entity);
-impl EntityBuilder {
-    fn new(id: Id) -> Self {
-        Self(Entity::new(id))
+pub struct Entity {
+    id_: EntityId,
+    properties: PropertyMap,
+}
+
+impl Entity {
+    pub fn new(id: EntityId) -> Self {
+        Self {
+            id_: id,
+            properties: PropertyMap::default(),
+        }
     }
 
     pub fn with(
@@ -40,12 +41,17 @@ impl EntityBuilder {
         property_name: impl Into<PropertyName>,
         property: impl Into<Property>,
     ) -> Self {
-        self.0.insert(property_name.into(), property.into());
+        self.properties
+            .insert(property_name.into(), property.into());
         self
     }
 
-    pub fn build(self) -> Entity {
-        self.0
+    pub fn id(&self) -> EntityId {
+        self.id_
+    }
+
+    pub fn get(&self, property_name: impl Into<PropertyName>) -> Option<&Property> {
+        self.properties.get(&property_name.into())
     }
 }
 
@@ -56,36 +62,58 @@ pub struct Query {
                                   //* discard: Vec<StoryBeat>, // TODO: some kind of identifier? uuid?
 }
 
-#[derive(Clone)]
+#[derive(Clone, Serialize, Deserialize)]
 pub struct ConstrainedAlias(pub Alias, pub Vec<PropertyConstraint>);
 
-#[derive(Default)]
+#[derive(Default, Serialize, Deserialize)]
 pub struct StoryNode {
     pub description: String,
     pub aliases: Vec<ConstrainedAlias>,
     pub relation_constraints: Vec<RelationConstraint>,
-    pub world_constraints: Vec<WorldConstraint>,
+    pub world_constraints: Vec<PropertyConstraint>,
     pub directives: Vec<String>, // TODO, some DSL instead of just strings? maybe this approach https://github.com/clap-rs/clap/blob/053c778e986d99b4f53afdb666d9398e75d8d2fb/examples/repl.rs
-    children: Vec<Rc<StoryNode>>,
 }
 
 impl StoryNode {
-    pub fn builder() -> StoryNodeBuilder {
-        StoryNodeBuilder::new()
+    pub fn new() -> Self {
+        Self::default()
     }
 
-    pub fn is_leaf(&self) -> bool {
-        self.children.is_empty()
+    // builder methods
+
+    pub fn with_description(mut self, description: impl Into<String>) -> Self {
+        self.description = description.into();
+        self
+    }
+
+    pub fn with_alias<T: Into<Alias>>(
+        mut self,
+        alias: T,
+        constraints: Vec<PropertyConstraint>,
+    ) -> Self {
+        self.aliases
+            .push(ConstrainedAlias(alias.into(), constraints));
+        self
+    }
+
+    pub fn with_relation<T: Into<Alias>>(
+        mut self,
+        me: T,
+        other: T,
+        constraint: PropertyConstraint,
+    ) -> Self {
+        self.relation_constraints.push(RelationConstraint {
+            me: me.into(),
+            other: other.into(),
+            constraint,
+        });
+        self
     }
 }
 
 pub struct StoryNodeBuilder(StoryNode);
 
 impl StoryNodeBuilder {
-    fn new() -> Self {
-        Self(StoryNode::default())
-    }
-
     pub fn with_description(mut self, description: impl Into<String>) -> Self {
         self.0.description = description.into();
         self
@@ -116,31 +144,25 @@ impl StoryNodeBuilder {
         self
     }
 
-    // TODO: Rework all this using a graph. perhaps petgraph??
-    pub fn with_sub_node(mut self, mut sub_node: StoryNode) -> Self {
-        sub_node.aliases.extend(self.0.aliases.clone());
-        sub_node
-            .relation_constraints
-            .extend(self.0.relation_constraints.clone());
-        sub_node
-            .world_constraints
-            .extend(self.0.world_constraints.clone());
-        self.0.children.push(Rc::new(sub_node));
-        self
-    }
+    // // TODO: Rework all this using a graph. perhaps petgraph??
+    // pub fn with_sub_node(mut self, mut sub_node: StoryNode) -> Self {
+    //     sub_node.aliases.extend(self.0.aliases.clone());
+    //     sub_node
+    //         .relation_constraints
+    //         .extend(self.0.relation_constraints.clone());
+    //     sub_node
+    //         .world_constraints
+    //         .extend(self.0.world_constraints.clone());
+    //     self.0.children.push(Rc::new(sub_node));
+    //     self
+    // }
 
     pub fn build(self) -> StoryNode {
         self.0
     }
 }
 
-impl Default for StoryNodeBuilder {
-    fn default() -> Self {
-        Self::new()
-    }
-}
-
-#[derive(Clone)]
+#[derive(Clone, Serialize, Deserialize)]
 pub enum PropertyConstraint {
     Has(PropertyName),
     IsInRange(PropertyName, Range<i64>),
@@ -160,7 +182,7 @@ impl PropertyConstraint {
     }
 }
 
-#[derive(Clone)]
+#[derive(Clone, Serialize, Deserialize)]
 pub struct RelationConstraint {
     pub me: Alias,
     pub other: Alias,
@@ -175,11 +197,6 @@ impl RelationConstraint {
             constraint,
         }
     }
-}
-
-#[derive(Clone)]
-pub enum WorldConstraint {
-    HasProperty(PropertyName),
 }
 
 // TODO: constraints that check at least, between, etc. fail on string prop
@@ -224,37 +241,75 @@ impl From<f64> for Property {
     }
 }
 
+// #[derive(Serialize, Deserialize)]
+#[derive(Default)]
+pub struct StoryGraph {
+    start_index: NodeIndex,
+    graph: Graph<StoryNode, f64>,
+}
+
+impl StoryGraph {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    pub fn start(&self) -> &StoryNode {
+        &self.graph[self.start_index]
+    }
+
+    pub fn start_with(&mut self, node_index: NodeIndex) {
+        self.start_index = node_index;
+    }
+
+    pub fn add(&mut self, story_node: StoryNode) -> NodeIndex {
+        self.graph.add_node(story_node)
+    }
+
+    pub fn connect(&mut self, from: NodeIndex, to: NodeIndex) {
+        self.connect_weight(from, to, 0.0);
+    }
+
+    pub fn connect_weight(&mut self, from: NodeIndex, to: NodeIndex, weight: f64) {
+        self.graph.add_edge(from, to, weight);
+    }
+}
+
+// #[derive(Serialize, Deserialize)]
 pub struct Raconteur {
-    story_beats: Vec<Rc<StoryNode>>,
+    stories: Vec<StoryGraph>,
 }
 
 impl Raconteur {
     pub fn new() -> Self {
-        Self {
-            story_beats: vec![],
-        }
+        Self { stories: vec![] }
     }
 
-    pub fn push(&mut self, story_beat: StoryNode) {
-        self.story_beats.push(Rc::new(story_beat))
+    pub fn insert(&mut self, story_graph: StoryGraph) {
+        self.stories.push(story_graph);
     }
 
     // Returns a pair of valid story beat with its list of valid aliased entities
     // inner vec is a list of permutations of indices. first index is for first alias, etc.
-    pub fn query(&self, query: &Query) -> Vec<(Rc<StoryNode>, Vec<Vec<usize>>)> {
+    pub fn query(&self, query: &Query) -> Vec<(StoryId, Vec<Vec<EntityIndex>>)> {
         // go through list of story beats, discarding those whose constraints aren't satisfied
 
-        self.story_beats
+        self.stories
             .iter()
-            .filter_map(|beat| {
-                let alias_candidates = are_all_constraints_satisfied(beat, query);
+            .enumerate()
+            .filter_map(|(story_idx, story_graph)| {
+                let story_node = &story_graph.graph[story_graph.start_index];
+                let alias_candidates = are_all_constraints_satisfied(story_node, query);
                 if alias_candidates.is_empty() {
                     None
                 } else {
-                    Some((Rc::clone(beat), alias_candidates))
+                    Some((StoryId(story_idx), alias_candidates))
                 }
             })
             .collect_vec()
+    }
+
+    pub fn get(&self, story_id: StoryId) -> &StoryGraph {
+        &self.stories[story_id.0]
     }
 }
 
@@ -264,22 +319,29 @@ impl Default for Raconteur {
     }
 }
 
-fn are_all_constraints_satisfied(beat: &Rc<StoryNode>, query: &Query) -> Vec<Vec<usize>> {
+fn are_all_constraints_satisfied(beat: &StoryNode, query: &Query) -> Vec<Vec<usize>> {
     if !are_world_constraints_satisfied(beat, query) {
         return vec![];
     }
     match_aliases(beat, query)
 }
 
-fn are_world_constraints_satisfied(beat: &Rc<StoryNode>, query: &Query) -> bool {
+fn are_world_constraints_satisfied(beat: &StoryNode, query: &Query) -> bool {
     beat.world_constraints
         .iter()
         .all(|constraint| match constraint {
-            WorldConstraint::HasProperty(prop_name) => query.world_state.contains_key(prop_name),
+            PropertyConstraint::Has(prop_name) => query.world_state.contains_key(prop_name),
+            PropertyConstraint::IsInRange(prop_name, range) => query
+                .world_state
+                .get(prop_name)
+                .is_some_and(|prop| match prop {
+                    Property::Int(value) => range.contains(value),
+                    _ => false,
+                }),
         })
 }
 
-fn match_aliases(beat: &Rc<StoryNode>, query: &Query) -> Vec<Vec<usize>> {
+fn match_aliases(beat: &StoryNode, query: &Query) -> Vec<Vec<usize>> {
     if query.entities.len() < beat.aliases.len() {
         return vec![];
     }
@@ -357,10 +419,8 @@ fn match_aliases(beat: &Rc<StoryNode>, query: &Query) -> Vec<Vec<usize>> {
                     .unwrap();
                 let me_idx = entity_indices[me_alias_idx];
                 let other_idx = entity_indices[other_alias_idx];
-                let me_id_prop = query.entities[me_idx].get(ID).unwrap().clone();
-                let other_id_prop = query.entities[other_idx].get(ID).unwrap().clone();
-                let Property::Int(me_id) = me_id_prop else {panic!()};
-                let Property::Int(other_id) = other_id_prop else {panic!()};
+                let me_id = query.entities[me_idx].id();
+                let other_id = query.entities[other_idx].id();
 
                 query
                     .entity_relations
--- tests/raconteur.rs
+++ tests/raconteur.rs
@@ -1,22 +1,15 @@
 #[cfg(test)]
 mod tests {
-    use std::rc::Rc;
-
     use raconteur::prelude::*;
 
-    const GUY_ID: i64 = 0;
-    const GIRL_ID: i64 = 1;
+    const GUY_ID: EntityId = EntityId::new(0);
+    const GIRL_ID: EntityId = EntityId::new(1);
 
     fn query() -> Query {
-        let character1 = Entity::builder(GUY_ID)
-            .with("name", "Bertrand")
-            .with("age", 30)
-            .build();
-        let character2 = Entity::builder(GIRL_ID)
+        let character1 = Entity::new(GUY_ID).with("name", "Bertrand").with("age", 30);
+        let character2 = Entity::new(GIRL_ID)
             .with("name", "Juliette")
-            .with("age", 32)
-            .build();
-
+            .with("age", 32);
         let relationships = RelationMap::from([(
             (GUY_ID, GIRL_ID),
             PropertyMap::from([("opinion".to_string(), 2.into())]),
@@ -31,57 +24,73 @@ mod tests {
 
     fn guy_no_like_girl() -> Raconteur {
         let mut raconteur = Raconteur::new();
-        raconteur.push(
-            StoryNode::builder()
-                .with_description("low_opinion")
-                .with_alias("guy", vec![])
-                .with_alias("girl", vec![])
-                .with_relation(
-                    "guy",
-                    "girl",
-                    PropertyConstraint::IsInRange(
-                        "opinion".to_string(),
-                        std::ops::Range { start: 0, end: 1 },
+        raconteur.insert({
+            let mut graph = StoryGraph::new();
+            let idx = graph.add(
+                StoryNode::new()
+                    .with_description("low_opinion")
+                    .with_alias("guy", vec![])
+                    .with_alias("girl", vec![])
+                    .with_relation(
+                        "guy",
+                        "girl",
+                        PropertyConstraint::IsInRange(
+                            "opinion".to_string(),
+                            std::ops::Range { start: 0, end: 1 },
+                        ),
                     ),
-                )
-                .build(),
-        );
+            );
+
+            graph.start_with(idx);
+
+            graph
+        });
 
         raconteur
     }
 
     fn guy_like_girl() -> Raconteur {
         let mut raconteur = Raconteur::new();
-        raconteur.push(
-            StoryNode::builder()
-                .with_description("guy_like_girl")
-                .with_alias("guy", vec![])
-                .with_alias("girl", vec![])
-                .with_relation(
-                    "guy",
-                    "girl",
-                    PropertyConstraint::IsInRange(
-                        "opinion".to_string(),
-                        std::ops::Range { start: 1, end: 4 },
+
+        raconteur.insert({
+            let mut graph = StoryGraph::new();
+            let idx = graph.add(
+                StoryNode::new()
+                    .with_description("guy_like_girl")
+                    .with_alias("guy", vec![])
+                    .with_alias("girl", vec![])
+                    .with_relation(
+                        "guy",
+                        "girl",
+                        PropertyConstraint::IsInRange(
+                            "opinion".to_string(),
+                            std::ops::Range { start: 1, end: 4 },
+                        ),
                     ),
-                )
-                .build(),
-        );
+            );
+
+            graph.start_with(idx);
+
+            graph
+        });
+
         raconteur
     }
 
     #[test]
     fn no_match() {
         let raconteur = guy_no_like_girl();
-        let beat_candidates = raconteur.query(&query());
-        assert!(beat_candidates.is_empty());
+        let story_candidates = raconteur.query(&query());
+        assert!(story_candidates.is_empty());
     }
 
     #[test]
     fn a_match() {
         let raconteur: Raconteur = guy_like_girl();
-        let beat_candidates = raconteur.query(&query());
-        let beat = Rc::clone(&beat_candidates[0].0);
-        assert_eq!(beat.description, "guy_like_girl");
+        let story_candidates = raconteur.query(&query());
+        let (story_id, _alias_candidates) = &story_candidates[0];
+        let story_graph = raconteur.get(*story_id);
+        let story_node = story_graph.start();
+        assert_eq!(story_node.description, "guy_like_girl");
     }
 }
